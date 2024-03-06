ed3f8e20d4d64d87766b01c960d7983c77a1fee8
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-03-06 16:38:03 +0530

    Implemented redux for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <ToastContainer />
+    </div>
   )
 }
 

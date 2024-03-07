a7cdc76c5ef4467255b563cfbd62e42861009c12
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-03-07 14:14:48 +0530

    created video card component

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { FcLike } from "react-icons/fc";
+import { FcComments } from "react-icons/fc";
+
+interface VideoCardProps {
+  thumbnail: string;
+  title: string;
+  userAvatar: string;
+  userName: string;
+  userHandle: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+const VideoCard: React.FC<VideoCardProps> = ({
+  thumbnail,
+  title,
+  userAvatar,
+  userName,
+  userHandle,
+  likeCount,
+  commentCount,
+}) => {
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700">
+      <img className="w-full rounded-t-3xl" src={thumbnail} alt="Video Thumbnail" />
+      <div className="px-6 py-4">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+        <div className="flex items-center mb-2 gap-2">
+          <img
+            className="w-10 h-10 rounded-full shadow-lg"
+            src={userAvatar}
+            alt={userName}
+          />
+          <div className="flex-1 min-w-0">
+            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+              {userName}
+            </p>
+            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+              {userHandle}
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-1">
+            <FcLike />
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {likeCount}
+            </p>
+          </div>
+          <div className="flex items-center gap-1">
+            <FcComments />
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {commentCount}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { AnyAction } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import { RootState } from "../store/store";
+import { useEffect, useState } from "react";
+import { getAllVideosApi } from "../store/videos.api";
+import VideoCard from "../components/VideoCard";
 
 const Home = () => {
+  const dispatch = useDispatch<ThunkDispatch<any, null, AnyAction>>()
+  const { loading, data } = useSelector((store: RootState) => store.videosManager); 
+  const [page, setPage] = useState<number>(3)
+  const [queryUrl, setQueryUrl] = useState<string>(`page=${page}`);
+
+  useEffect(() => {
+    dispatch(getAllVideosApi(queryUrl));
+  }, [queryUrl, dispatch]);
+
+  console.log("data:", data)
   return (
-    <div>Home</div>
+    <div className="grid grid-cols-3 gap-10 items-center justify-center w-11/12 m-auto">
+      {data?.posts?.map((video) => (
+        <VideoCard key={video.postId} thumbnail={video.submission.thumbnail} title={video.submission.title} userAvatar={video.creator.pic} userName={video.creator.name} userHandle={video.creator.handle} likeCount={video.reaction.count} commentCount={video.comment.count} />
+      ))}
+    </div>
   )
 }
 
